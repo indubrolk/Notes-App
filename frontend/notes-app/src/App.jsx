2bf50804b30351d602d2f0743fbb1582417b3a22
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-08 09:47:25 +0530

    app,login,signup created

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './pages/Home/Home'
+import Home from './pages/Home/Home.jsx'
+import Login from './pages/Login/Login.jsx'
+import SignUp from './pages/SignUp/SignUp.jsx'
 
 const routes = (
     <Router>
