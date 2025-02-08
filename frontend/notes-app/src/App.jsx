e1af3cd6f651d5929d565e48dc523815b614d00b
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-08 09:15:57 +0530

    app.jsx setuped

--- frontend/notes-app/src/App.jsx
+++ frontend/notes-app/src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home/Home'
 
-const App = () => {
-    return (
-        <div>
-            <Home />
-        </div>
-    )
-}
+const routes = (
+    <Router>
+        <Routes>
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<SignUp />} />
+        </Routes>
+    </Router>
+);
 
-export default App
+const App = () => {
+    return <div>{routes}</div>;
+};
+
+export default App;
