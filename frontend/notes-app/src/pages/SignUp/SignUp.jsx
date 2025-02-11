9bb3f2094cba4c2ef98e65e2aceba63fd548c40b
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-11 10:10:36 +0530

    sign up edited

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Navbar from "../../components/Navbar.jsx";
+import {Link} from "react-router-dom";
 
 const SignUp = () => {
 
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
     const handleSignUp = async (e) => {
         e.preventDefault();
@@ -21,9 +24,31 @@ const SignUp = () => {
 
                         <input
                             type="text"
+                            placeholder="Name"
+                            className="input-box"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+
+                        <input
+                            type="text"
                             placeholder="Email"
                             className="input-box"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
                         />
+
+                       <PasswordInput
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                       />
+
+                        <p className="text-sm text-center mt-4">
+                            Already have an account?{" "}
+                            <Link to="/Signup" className="font-medium text-blue-500 underline">
+                                Login
+                            </Link>
+                        </p>
                     </form>
                 </div>
             </div>
