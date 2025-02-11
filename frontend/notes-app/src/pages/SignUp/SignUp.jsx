ed1863857ce222039ffd78aab382e617ae97c039
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-11 12:22:14 +0530

    Navbar.jsx created

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/Navbar.jsx";
+import Navbar from "../../components/Navbar/Navbar.jsx";
 import {Link} from "react-router-dom";
 import {validateEmail} from "../../utils/helper.js";
 
