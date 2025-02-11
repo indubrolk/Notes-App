4874985e2b0b7005d2c2631063bbf1153edf9a2e
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-11 12:38:05 +0530

    missing imports fixed

--- frontend/notes-app/src/pages/SignUp/SignUp.jsx
+++ frontend/notes-app/src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar.jsx";
+import {useState} from "react";
 import {Link} from "react-router-dom";
 import {validateEmail} from "../../utils/helper.js";
 
