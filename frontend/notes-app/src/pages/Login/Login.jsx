f901edd47427911511c630f680887ffbc0ffd2ca
Kavindu <wkanjana1590@gmail.com>
2025-02-12 14:56:10 +0530

    Navbar updates and SearchBar creation

--- frontend/notes-app/src/pages/Login/Login.jsx
+++ frontend/notes-app/src/pages/Login/Login.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import Navbar from "../../components/Navbar/Navbar.jsx";
 import {Link} from "react-router-dom";
+import PasswordInput from "../../components/input/PasswordInput.jsx";
 
 
 const Login = () => {
+
+     const [email, setEmail] = React.useState("");
+     const [password, setPassword] = React.useState("");
+     const [error, setError] = React.useState(null);
+
  return (
      <>
      <Navbar />
@@ -11,7 +17,11 @@ const Login = () => {
              <div className="w-96 border rounded bg-white px-7 py-10">
                  <form onSubmit={() => {}}>
                      <h4 className="text-2xl mb-7">Login</h4>
-                     <input type="text" placeholder="Email" className="input-box"/>
+                     <input type="text" placeholder="Email" className="input-box"
+                            value = {email}
+                            onChange={(e) => setEmail(e.target.value)}/>
+                     <PasswordInput />
+
                      <button type="submit" className="btn-primary">
                          Login
                      </button>
