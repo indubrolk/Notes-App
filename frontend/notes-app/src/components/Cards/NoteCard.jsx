84794268c2dc32b4bfeab67d909eda2db9c61f31
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-13 08:59:05 +0530

    NoteCard.jsx edited

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {MdOutlinePushPin} from "react-icons/md";
+import {MdCreate, MdDelete} from "react-icons/md";
 
 
 const NoteCard = (
@@ -18,10 +20,17 @@ const NoteCard = (
         <div>
             <div>
                 <div>
-                    <h6 className="text-sm font-medium"></h6>
+                    <h6 className="text-sm font-medium">{title}</h6>
                     <span className="text-xs text-slate-500">{date}</span>
                 </div>
+
+                <MdOutlinePushPin className="" onClick={onPinNote}/>
             </div>
+
+            <p className="">{content?.slice(0, 60)}</p>
+
+            <div></div>
+
         </div>
 
     );
