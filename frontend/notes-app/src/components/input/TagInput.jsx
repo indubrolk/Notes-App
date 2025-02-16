f701bf7f377d7ce2823bb4787a804c250d4c1550
Kavindu <wkanjana1590@gmail.com>
2025-02-16 12:29:54 +0530

    note editor's closing button

--- frontend/notes-app/src/components/input/TagInput.jsx
+++ frontend/notes-app/src/components/input/TagInput.jsx
@@ -27,9 +27,9 @@ const TagInput = ({tags,setTags}) => {
             {tags?.length > 0 && (
             <div className="flex items-center gap-2 flex-wrap mt-2">
                 {tags.map((tag,index) => (
-                <span key={index} className="">
+                <span key={index} className="flex items-center gap-2 text-sm text-slate-900 bg-slate-100 px-3 py-1 rounded-xl">
                     # {tag}
-                    <button onClick={() => {}}>
+                    <button onClick={() => handleRemoveTag(tag)}>
                         <MdClose />
                     </button>
                 </span>
@@ -38,7 +38,9 @@ const TagInput = ({tags,setTags}) => {
             )}
 
             <div className="flex items-center gap-4 mt-3">
-                <input type="text" className="text-sm bg-transparent border px-3 py-2 rounded outline-none " placeholder="Add tags"
+                <input type="text"
+                 value={inputValue}
+                       className="text-sm bg-transparent border px-3 py-2 rounded outline-none " placeholder="Add tags"
                 onChange={handleInputChange}
                 onKeyDown={handleKeyDown}
                 />
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -57,7 +57,11 @@ const Home = () => {
                 className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-15 overflow-scroll"
                 >
 
-                <AddEditNotes />
+                <AddEditNotes
+                    onClose={() => {
+                        setOpenAddEditModal({ isShown: false, type: "add", data: null });
+                    }}
+                />
             </Modal>
         </>
     );
