52965b16ec0b43a73100d2eb0204c14d384e05e5
indubrolk <110773586+indubrolk@users.noreply.github.com>
2025-02-18 12:01:13 +0530

    backend setuped

---
+++ backend/utillities.js
@@ -0,0 +1,8 @@
+const jwt = require('jsonwebtoken')
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split("")[1];
+
+    if (!token)
+}
