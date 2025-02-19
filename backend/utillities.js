a414c9e384907d3e65f4d66812b2100fd3e2f22b
Kavindu <wkanjana1590@gmail.com>
2025-02-19 20:26:31 +0530

    backend utilities

--- backend/utillities.js
+++ backend/utillities.js
@@ -4,5 +4,13 @@ function authenticateToken(req, res, next){
     const authHeader = req.headers["authorization"];
     const token = authHeader && authHeader.split("")[1];
 
-    if (!token)
+    if (!token) return res.sendStatus(401);
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(401);
+        req.user = user;
+        next();
+    });
 }
+module.exports = {
+    authenticateToken,
+};
